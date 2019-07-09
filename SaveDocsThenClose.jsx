fcf254e650ef9743c5267c9807b816a0a7ff98c1
vianneymiranda <vianneymiranda@thecustomsite.com>
2019-07-09 08:58:23 -0400

    Quick scripts use for carding in different language

---
+++ SaveDocsThenClose.jsx
@@ -0,0 +1,20 @@
+﻿for (i=0; i<app.documents.length; i++)  
+    {  
+        var idoc = app.documents[i];  
+        //alert("+"+idoc.path+"+");  
+        if (idoc.path == "")  
+            {  
+                file = File.saveDialog ("Save file...");  
+                idoc.saveAs (file);  
+            }  
+        else  
+            {  
+                //alert(idoc.name);  
+                if (!idoc.saved)  
+                    {   
+                        idoc.save();  
+                        idoc.saved = true;
+                        idoc.close();  
+                    }  
+            }  
+    }  
